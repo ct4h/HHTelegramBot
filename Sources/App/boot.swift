149d846747f2f895c2e8038fd72d2136e14b3d87
basalaev <basalaev.dev@gmail.com>
2019-01-27 18:04:59 +0300

    Add restart bot after error

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,12 +1,27 @@
 import Vapor
 import Telegrammer
 
+private var botService: RedmineBot?
+
 public func boot(_ app: Application) throws {
+    botService = try app.make(RedmineBot.self)
+    try start(bot: botService)
+}
 
-    let botService = try app.make(RedmineBot.self)
+private func start(bot: RedmineBot?) throws {
+    guard let bot = bot else {
+        return
+    }
 
-    /// Starting longpolling way to receive bot updates
-    /// Or either use webhooks by calling `startWebhooks()` method instead
-    _ = try botService.updater?.startLongpolling()
+    print("Start bot")
 
+    let pollingPromise = try bot.updater?.startLongpolling()
+    pollingPromise?.catch({ error in
+        print("Longpolling error \(error)")
+        do {
+            try start(bot: bot)
+        } catch {
+            print("Fail restart bot")
+        }
+    })
 }
